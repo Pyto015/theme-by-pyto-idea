f784a221e3ec6afff5adef3dae398c023017435f
GitHub Action <action@github.com>
2023-01-28 14:01:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/pyto015/themebypytoidea/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/pyto015/themebypytoidea/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.pyto015.themebypytoidea.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.pyto015.themebypytoidea.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/pyto015/themebypytoidea/services/MyProjectService.kt
+++ src/main/kotlin/com/github/pyto015/themebypytoidea/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.pyto015.themebypytoidea.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.pyto015.themebypytoidea.MyBundle
 
 class MyProjectService(project: Project) {
 
